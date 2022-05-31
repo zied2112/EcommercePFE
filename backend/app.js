4f866a8ff9adbc009f720a107405c2839330b0b6
Zied Ben Amor <zied-ci@hotmail.fr>
2022-05-31 12:26:16 +0100

    new one

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,14 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 }
 
 
-
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(fileUpload())
 app.use(cors())
 app.use(express.json())
